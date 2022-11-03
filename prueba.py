08e7af574a9d80ef64769cb11e7deeaf20c4ecd2
PinoCoelho <josethherra@gmail.com>
2022-11-02 23:33:42 -0600

    scene

--- prueba.py
+++ prueba.py
@@ -60,6 +60,68 @@ class escenario1:
         self.canvas.pack(fill = BOTH, expand = True)
 
         self.master.mainloop()
+
+class escenario2:
+
+    def __init__(self, master = None):
+        self.master = master
+        
+        self.create()
+ 
+    def create(self):
+ 
+        self.master = Tk()
+
+        self.imagen_robot = PhotoImage (file = 'Images/robot.png')
+        self.imagen_cono = PhotoImage (file = 'Images/cono.png')
+        self.imagen_arbol = PhotoImage (file = 'Images/arbol.png')
+    
+        self.master.title("ESCENARIO 2")
+ 
+    
+        self.master.minsize(502,502) 
+ 
+    
+    
+        self.canvas = Canvas(self.master)
+
+        #Horizontales
+        
+        self.canvas.create_line(700, 100, 0, 100)
+
+        self.canvas.create_line(700, 200, 0, 200)
+
+        self.canvas.create_line(700, 300, 0, 300)
+
+        self.canvas.create_line(700, 400, 0, 400)
+
+        self.canvas.create_line(700, 500, 0, 500)
+
+        self.canvas.create_line(700, 600, 0, 600)
+ 
+        #verticales
+
+        self.canvas.create_line(500, 0, 500, 700)
+         
+        self.canvas.create_line(400, 0, 400, 700)
+
+        self.canvas.create_line(300, 0, 300, 700)
+        
+        self.canvas.create_line(200, 0, 200, 700)
+        
+        self.canvas.create_line(100, 0, 100, 700) 
+        
+        #Imagenes
+
+        self.canvas.create_image(250,250,image = self.imagen_robot)
+
+        self.canvas.create_image(250,50,image = self.imagen_cono)
+
+        self.canvas.pack(fill = BOTH, expand = True)
+
+        self.canvas.create_image(150,400,image = self.imagen_arbol)
+
+        self.master.mainloop()
 """        
 if __name__ == "__main__":
     
@@ -75,4 +137,5 @@ if __name__ == "__main__":
     
     master.mainloop()
 """
+escenario2()
 #escenario1()
