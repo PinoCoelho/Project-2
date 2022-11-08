fb60212dce625228095c95d35ff44de4e640e27f
PinoCoelho <josethherra@gmail.com>
2022-11-07 23:33:53 -0600

    Últimas modificaciones

--- Escenarios.py
+++ Escenarios.py
@@ -3,7 +3,7 @@ from tkinter.ttk import *
  
 class escenario1:
 
-    def __init__(self):
+    def __init__(self): #El constructor de la clase
         """
                 Instituto Tecnológico de Costa Rica
                     Ingeniería en Computadores
@@ -17,7 +17,7 @@ class escenario1:
 
         """
 
-        self.create ()
+        self.create () #Se llama a la función
  
     def create(self):
         """
@@ -33,22 +33,24 @@ class escenario1:
 
         """
  
-        self.master = Tk()
+        self.master = Tk() #Se inicia Tkinter
+
+        #Imágenes
 
         self.imagen_robot = PhotoImage (file = 'Images/robot.png')
         self.imagen_cono = PhotoImage (file = 'Images/cono.png')
         self.imagen_arbol = PhotoImage (file = 'Images/arbol.png')
     
-        self.master.title("ESCENARIO 1")
+        self.master.title("ESCENARIO 1") #Título de la pantalla
  
     
-        self.master.minsize(502,502) 
-        self.master.resizable(False,False)
+        self.master.minsize(502,502) #Dimensiones de la pantalla
+        self.master.resizable(False,False) #Que no se puedan modificar
     
     
-        self.canvas = Canvas(self.master)
+        self.canvas = Canvas(self.master) #Se crea el canvas
 
-        #Horizontales
+        #Líneas Horizontales
         
         self.canvas.create_line(700, 100, 0, 100)
 
@@ -62,7 +64,7 @@ class escenario1:
 
         self.canvas.create_line(700, 600, 0, 600)
  
-        #verticales
+        #Líneas Verticales
 
         self.canvas.create_line(500, 0, 500, 700)
          
@@ -73,7 +75,8 @@ class escenario1:
         self.canvas.create_line(200, 0, 200, 700)
         
         self.canvas.create_line(100, 0, 100, 700)
-        #Imagenes
+
+        #Poner las imágenes
 
         self.robot = self.canvas.create_image(50,50,image = self.imagen_robot,tags ="self.robot")
 
@@ -81,75 +84,79 @@ class escenario1:
 
         self.canvas.pack(fill = BOTH, expand = True)
 
+        #Variables globales
+
         global temperatura1
         global temperatura2
         temperatura1 = 20
         temperatura2 = 10
 
-        def medidor_cono (self):
-            #global temperatura1
-            file = open(r"mediciones.txt", 'a')
-            file.write(str(temperatura1)+"\n")
-            promedio(self)
-            file.close
+        def medidor_cono (self): #Función que mide la temperatura en el sitio del cono
+            file = open(r"mediciones.txt", 'a') #Abre el archivo
+            file.write(str(temperatura1)+"\n") #Escribe en el archivo
+            file.close #Cierra el archivo
+            promedio(self) #Llama a la función promedio
 
-        def medidor_abajo (self):
-            file = open(r"mediciones.txt", 'a')
-            file.write(str(temperatura2)+"\n")
-            file.close
-            promedio(self)
+        def medidor_abajo (self): #Función que mide la temperatura en el sitio de abajo
+            file = open(r"mediciones.txt", 'a') #Abre el archivo
+            file.write(str(temperatura2)+"\n") #Escribe en el archivo
+            file.close #Cierra el archivo
+            promedio(self) #Llama a la función promedio
 
-        def promedio (self):
-           promedio = temperatura1 + temperatura2
+        def promedio (self): #Función que saca el promedio
+           promedio = (temperatura1 + temperatura2) // 10
            print(promedio)
 
-        def colision_cono (self):
-            pos1 = self.canvas.bbox(self.robot)
-            pos2 = self.canvas.bbox(self.cono)
-            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]:
-                medidor_cono(self)
-                print ("¡Colisión Cono!")
+        def colision_cono (self): #Función que crea la colisión entre el robot y el cono
+            pos1 = self.canvas.bbox(self.robot) #Posición del robot
+            pos2 = self.canvas.bbox(self.cono) #Posición del cono
+            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]: #Para ver las posiciones y ver si chocan
+                medidor_cono(self) #Llama a la función medidor_cono
+                print ("¡Colisión Cono!") #Manda un print de que hubo una colisión
 
-        def move_left(temp):
-            x1,y1,x2,y2=self.canvas.bbox("self.robot")
-            if(x1<=0):
-                return
+        def move_left(temp): #Función que hace que el robot se mueva hacia la izquierda
+            x1,y1,x2,y2=self.canvas.bbox("self.robot") #Posiciones del robot
+            if(x1<=0): #Si x1 es igual o menor a 0 entonces llegó al límite de la pantalla
+                print("Chocó a la izquierda") #Pone uun print de que chocó abajo
             else:
-                self.canvas.move(self.robot,-5,0)
-                colision_cono(self)
+                self.canvas.move(self.robot,-5,0) #Hace que el robot se mueva hacia la izquierda
+                colision_cono(self) #Llama a colisión cono
                 #print("Left")
-        def move_right(temp):
-            x1,y1,x2,y2=self.canvas.bbox("self.robot")
-            if(x2>=self.canvas.winfo_width()-5):
-                return
+
+        def move_right(temp): #Función que hace que el robot se mueva hacia la derecha
+            x1,y1,x2,y2=self.canvas.bbox("self.robot") #Posiciones del robot
+            if(x2>=self.canvas.winfo_width()-5):#Si x2 es igual o mayor al largo de la ventana entonces llegó al límite de la pantalla
+                print("Chocó a la derecha") #Pone uun print de que chocó abajo
             else:
-                self.canvas.move(self.robot,5,0)
-                colision_cono(self)
+                self.canvas.move(self.robot,5,0) #Hace que el robot se mueva hacia la derecha
+                colision_cono(self) #Llama a colisión cono
                 #print("Right")  
-        def move_up(temp):
-            x1,y1,x2,y2=self.canvas.bbox("self.robot")
-            if(y1<=0):
-                return
+
+        def move_up(temp): #Función que hace que el robot se mueva hacia arriba
+            x1,y1,x2,y2=self.canvas.bbox("self.robot") #Posiciones del robot
+            if(y1<=0): #Si y1 es igual o menor a 0 entonces llegó al límite de la pantalla
+                print("Chocó arriba") #Pone uun print de que chocó abajo
             else:
-                self.canvas.move(self.robot,0,-5) 
-                colision_cono(self)
+                self.canvas.move(self.robot,0,-5) #Hace que el robot se mueva hacia arriba
+                colision_cono(self) #Llama a colisión cono
                 #print("Up")  
-        def move_down(temp):
-            x1,y1,x2,y2=self.canvas.bbox("self.robot")
-            if(y2>=self.canvas.winfo_height()-5):
-                medidor_abajo(self)
-                print("Chocó abajo")
+
+        def move_down(temp): #Función que hace que el robot se mueva hacia arriba
+            x1,y1,x2,y2=self.canvas.bbox("self.robot") #Posiciones del robot
+            if(y2>=self.canvas.winfo_height()-5): #Si y2 es igual o mayor al largo de la ventana entonces llegó al límite de la pantalla
+                medidor_abajo(self) #Llama a medidor abajo
+                print("Chocó abajo") #Pone uun print de que chocó abajo
             else:
-                self.canvas.move(self.robot,0,5)  
-                colision_cono(self)
+                self.canvas.move(self.robot,0,5)   #Hace que el robot se mueva hacia abajo
+                colision_cono(self) #Llama a colision cono
                 #print("Down")  
         
-        self.master.bind('d', move_right)
-        self.master.bind('a', move_left)
-        self.master.bind('w', move_up)
-        self.master.bind('s', move_down)
+        self.master.bind('d', move_right) #Para que se mueva hacia la derecha con la letra d
+        self.master.bind('a', move_left) #Para que se mueva hacia la izquierda con la letra a
+        self.master.bind('w', move_up) #Para que se mueva hacia arriba con la letra w
+        self.master.bind('s', move_down) #Para que se mueva hacia abajo con la letra s
 
-        self.master.mainloop()
+        self.master.mainloop() #Mainloop de la pantalla
 
 class escenario2:
     """
@@ -165,7 +172,7 @@ class escenario2:
 
     """
 
-    def __init__(self, master = None):
+    def __init__(self, master = None): #Constructor de la clase
         """
                 Instituto Tecnológico de Costa Rica
                     Ingeniería en Computadores
@@ -179,10 +186,9 @@ class escenario2:
 
         """
 
-        self.master = master
-        self.create()
+        self.master = master #Se establece el self.master
+        self.create() #Se manda a crear la función create
         self.imprimir()
-        self.temp = 0
 
 
     def create(self):
@@ -199,22 +205,24 @@ class escenario2:
 
         """
  
-        self.master = Tk()
+        self.master = Tk() #Se inicializa Tkinter
+
+        #Imágenes
 
         self.imagen_robot = PhotoImage (file = 'Images/robot.png')
         self.imagen_cono = PhotoImage (file = 'Images/cono.png')
         self.imagen_arbol = PhotoImage (file = 'Images/arbol.png')
     
-        self.master.title("ESCENARIO 2")
+        self.master.title("ESCENARIO 2") #Título de la pantalla
  
     
-        self.master.minsize(502,502) 
-        self.master.resizable(False,False)
+        self.master.minsize(502,502) #Dimensiones de la pantalla
+        self.master.resizable(False,False) #No se puede modificar
     
     
-        self.canvas = Canvas(self.master)
+        self.canvas = Canvas(self.master) #Se crea el canvas
 
-        #Horizontales
+        #Líneas Horizontales
         
         self.canvas.create_line(700, 100, 0, 100)
 
@@ -228,7 +236,7 @@ class escenario2:
 
         self.canvas.create_line(700, 600, 0, 600)
  
-        #verticales
+        #Líneas Verticales
 
         self.canvas.create_line(500, 0, 500, 700)
          
@@ -240,7 +248,7 @@ class escenario2:
         
         self.canvas.create_line(100, 0, 100, 700) 
         
-        #Imágenes
+        #Se ponen las imágenes
 
         self.robot = self.canvas.create_image(250,250,image = self.imagen_robot, tags=("self.robot"))
 
@@ -248,96 +256,96 @@ class escenario2:
 
         self.arbol = self.canvas.create_image(150,400,image = self.imagen_arbol)
 
-        self.canvas.pack(fill = BOTH, expand = True)
+        self.canvas.pack(fill = BOTH, expand = True) #Se crea el canvas
+
+        #Variables globales
 
         global temperatura1
         global temperatura2
         temperatura1 = 20
         temperatura2 = 10
 
-        def medidor_cono (self):
-            #global temperatura1
-            file = open(r"mediciones.txt", 'a')
-            file.write(str(temperatura1)+"\n")
-            promedio(self)
-            file.close
+        def medidor_cono (self): #Medidor de la temperatura
+            file = open(r"mediciones.txt", 'a') #Se abre el archivo
+            file.write(str(temperatura1)+"\n") #Se escribe en el archivo
+            file.close #Se cierra el archivo
+            promedio(self) #LLama a la función promedio
 
-        def medidor_borde_derecho (self):
-            file = open(r"mediciones.txt", 'a')
-            file.write(str(temperatura2)+"\n")
-            file.close
-            promedio(self)
+        def medidor_borde_derecho (self): #Medidor de la temperatura
+            file = open(r"mediciones.txt", 'a') #Se abre el archivo
+            file.write(str(temperatura2)+"\n") #Se escribe en el archivo
+            file.close #Se cierra el archivo
+            promedio(self) #LLama a la función promedio
 
-        def promedio (self):
-           promedio = temperatura1 + temperatura2
+        def promedio (self): #Función que saca el promedio
+           promedio = (temperatura1 + temperatura2) // 5
            print(promedio)
 
         #self.menubar = Menu(self.master) #Se crea la barra del menú.
         #self.menubar.add_command(label="PRESENTACIÓN",command= print("Ola")) #Se crea el comando para que.
         #self.master.config(menu=self.menubar) #Se posiciona la barra del menú.
 
-        def colision_cono (self):
-            pos1 = self.canvas.bbox(self.robot)
-            pos2 = self.canvas.bbox(self.cono)
-            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]:
-                medidor_cono(self)
-                print ("¡Colisión Cono!")
+        def colision_cono (self): #Función que saca la colisión con el cono
+            pos1 = self.canvas.bbox(self.robot) #Posición del robot
+            pos2 = self.canvas.bbox(self.cono) #Posici+on del cono
+            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]: #Se ve si pegan
+                medidor_cono(self) #Llama medidor cono
+                print ("¡Colisión Cono!") #Pone un print de que colisionó
 
-        def colision_arbol (self):
-            pos1 = self.canvas.bbox(self.robot)
-            pos2 = self.canvas.bbox(self.arbol)
-            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]:
-                print ("¡Colisión Árbol!")
+        def colision_arbol (self): #Función que saca la colisión con el árbol
+            pos1 = self.canvas.bbox(self.robot) #Posición del robot
+            pos2 = self.canvas.bbox(self.arbol) #Posici+on del árbol
+            if pos2[0] < pos1[2] < pos2[2] and pos2[1] < pos1[1] < pos2[3]: #Se ve si pegan
+                print ("¡Colisión Árbol!") #Pone un print de que colisionó
 
         #Movimientos del robot
 
-        def move_left(temp):
-            x1, y1, x2, y2 = self.canvas.bbox("self.robot")
-            if(x1 <= 0):
-                return
+        def move_left(temp): #Función para que se mueva hacia la izquierda
+            x1, y1, x2, y2 = self.canvas.bbox("self.robot") #Posiciones del canvas
+            if(x1 <= 0): #Si x1 es menor o igual a 0 entonces llegó al límite de la pantalla
+                print("Chocó a la izquierda") #Pone un print de que chocó a la izquierda
             else:
-                self.canvas.move(self.robot, -5, 0)
-                colision_cono(self)
-                colision_arbol(self)
+                self.canvas.move(self.robot, -5, 0) #El robot se mueve
+                colision_cono(self) #Llama a colisión cono
+                colision_arbol(self) #LLama a colisión árbol
                 #print("Left")
 
-
-        def move_right(temp):
-            x1, y1, x2, y2 = self.canvas.bbox("self.robot")
-            if(x2 >= self.canvas.winfo_width() -5):
-                medidor_borde_derecho(self)
-                print("Chocó con el borde derecho")
+        def move_right(temp): #Función para que se mueva hacia la derecha
+            x1, y1, x2, y2 = self.canvas.bbox("self.robot") #Posiciones del canvas
+            if(x2 >= self.canvas.winfo_width() -5): #Si x2 es mayor o igual al largo de la pantalla entonces llegó al límite de la pantalla
+                medidor_borde_derecho(self) #Llama al medidor
+                print("Chocó a la derecha") #Print de que chocó
             else:
-                self.canvas.move(self.robot, 5, 0)
-                colision_cono(self)
-                colision_arbol(self)
+                self.canvas.move(self.robot, 5, 0) #El robot se mueve
+                colision_cono(self) #Llama a colisión cono
+                colision_arbol(self) #LLama a colisión árbol
                 #print("Right")
 
-        def move_up(temp):
-            x1, y1, x2, y2 = self.canvas.bbox("self.robot")
-            if(y1 <= 0):
-                return
+        def move_up(temp): #Función para que se mueva hacia arriba
+            x1, y1, x2, y2 = self.canvas.bbox("self.robot") #Posiciones del canvas
+            if(y1 <= 0): #Si y1 es menor o igual a 0 entonces llegó al límite de la pantalla
+                print("Chocó arriba") #Pone uun print de que chocó arriba
             else:
-                self.canvas.move(self.robot, 0, -5)
-                colision_cono(self)
-                colision_arbol(self)
+                self.canvas.move(self.robot, 0, -5) #El robot se mueve
+                colision_cono(self) #Llama a colisión cono
+                colision_arbol(self) #LLama a colisión árbol
                 #print("Up") 
 
-        def move_down(temp):
-            x1, y1, x2, y2 = self.canvas.bbox("self.robot")
-            if(y2 >= self.canvas.winfo_height() -5):
-                return
+        def move_down(temp): #Función para que se mueva hacia arriba
+            x1, y1, x2, y2 = self.canvas.bbox("self.robot") #Posiciones del canvas
+            if(y2 >= self.canvas.winfo_height() -5): #Si y2 es mayor o igual al largo de la pantalla entonces llegó al límite de la pantalla
+                print("Chocó abajo") #Pone uun print de que chocó abajo
             else:
-                self.canvas.move(self.robot, 0, 5)
-                colision_cono(self)
-                colision_arbol(self)
+                self.canvas.move(self.robot, 0, 5) #El robot se mueve
+                colision_cono(self) #Llama a colisión cono
+                colision_arbol(self) #LLama a colisión árbol
                 #print("Down")
         
-        self.master.bind('d', move_right)
-        self.master.bind('a', move_left)
-        self.master.bind('w', move_up)
-        self.master.bind('s', move_down)
-        self.master.mainloop()
+        self.master.bind('d', move_right) #Hace que se mueva hacia la derecha con la letra d
+        self.master.bind('a', move_left) #Hace que se mueva hacia la izquierda con la letra a
+        self.master.bind('w', move_up) #Hace que se mueva hacia arriba con la letra w
+        self.master.bind('s', move_down) #Hace que se mueva hacia abajo con la letra s
+        self.master.mainloop() #El mainloop de la pantalla
     
     def imprimir (self):
         print (escenario2.__doc__())
--- Principal.py
+++ Principal.py
@@ -18,7 +18,7 @@ class iniciador:
 
         """
 
-        self.inicio()
+        self.inicio() #llama a la función inicio
         self.scene = iniciador.scene
 
     def cargarImagen(self,nombre): #Funcion para cargar la imagen. 
@@ -37,7 +37,7 @@ class iniciador:
 
         self.ruta = os.path.join('Images',nombre) #Se define la ubicación de la imagen.
         imagen = PhotoImage(file=self.ruta)
-        return imagen
+        return imagen #Retorna la imagen
 
     
     def inicio (self):
@@ -60,34 +60,32 @@ class iniciador:
         self.ventana.resizable(width=NO,height=NO) #Que no se puede modificar. 
         self.fondo = iniciador.cargarImagen(self,"robot_fondo2.gif") #Pone la imagen de fondo. 
         self.canvas = Canvas(self.ventana, width=700, height=700, bg="purple")#Crea el canvas
-        self.canvas.place(x=0, y=0)
-        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw")
-        # self.LabelFondo=Label(self.ventana, image=self.fondo) #Etiqueta que va a contener la imagen.
-        # self.LabelFondo.place (x=0, y=0) #Posición de la imagen.
-        self.Boton_iniciar = Button(self.ventana,text="INICIAR",bg="#33AA5C",fg="black",height= 2,width=7,command= self.scene) #Botón para entrar a la pantalla secundaria llamada game.
+        self.canvas.place(x=0, y=0) #Lo posiciona
+        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw") #Crea una imagen en el canvas
+        self.Boton_iniciar = Button(self.ventana,text="INICIAR",bg="#33AA5C",fg="black",height= 2,width=7,command= self.scene) #Botón para entrar a la pantalla secundaria llamada game
         self.Boton_iniciar.place(x=308,y=450, anchor = "center")
         self.Boton_salir = Button (self.ventana,text="APAGAR", bg="#33AA5C",fg="black",height= 2,width=7,command= self.ventana.destroy) #Boton para apagar el programa
         self.Boton_salir.place (x= 510,y= 20)
         self.Boton_about = Button (self.ventana,text="ABOUT", bg="#33AA5C",fg="black",height= 2,width=7,command= self.about) #Boton pantalla about
         self.Boton_about.place (x= 50,y= 20)
-        self.Boton_presentacion = Button (self.ventana,text="PRESENTACION", bg="#33AA5C",fg="black",height= 2,width=12) #Boton de presentación
+        self.Boton_presentacion = Button (self.ventana,text="PRESENTACION", bg="#33AA5C",fg="black",height= 2,width=12,command = print ("Hola. Soy Pino, modelo 1.5 y trabajo para el TEC. He efectuado un total de 10 mediciones de temperatura")) #Boton de presentación
         self.Boton_presentacion.place (x= 120,y= 20)
-        self.Label_text_nombre = Label (self.ventana, text="Por favor introduce un nombre al robot:", bg = "#67B59C", fg = "black")
-        self.Label_text_nombre.place (x= 310,y=200,anchor="center")
-        self.Entry_nombre = Entry(self.ventana, width=25, bg="light green")
-        self.Entry_nombre.place (x= 310,y= 230,anchor="center")
-        self.Label_text_creacion = Label (self.ventana, text="Por favor introduce la fecha de creación del robot:", bg = "#67B59C", fg = "black")
-        self.Label_text_creacion.place (x= 310,y=260,anchor="center")
-        self.Entry_creacion = Entry(self.ventana, width=25, bg="light green")
-        self.Entry_creacion.place (x= 310,y= 290,anchor="center")
-        self.Label_text_modelo = Label (self.ventana, text="Por favor introduce un modelo al robot:", bg = "#67B59C", fg = "black")
-        self.Label_text_modelo.place (x= 310,y=320,anchor="center")
-        self.Entry_modelo = Entry(self.ventana, width=25, bg="light green")
-        self.Entry_modelo.place (x= 310,y= 350,anchor="center")
-        self.Label_text_fabricante = Label (self.ventana, text="Por favor introduce el nombre del fabricante:", bg = "#67B59C", fg = "black")
-        self.Label_text_fabricante.place (x= 310,y=380,anchor="center")
-        self.Entry_fabricante = Entry(self.ventana, width=25, bg="light green")
-        self.Entry_fabricante.place (x= 310,y= 410,anchor="center")
+        self.Label_text_nombre = Label (self.ventana, text="Por favor introduce un nombre al robot:", bg = "#67B59C", fg = "black") #Label con un texto
+        self.Label_text_nombre.place (x= 310,y=200,anchor="center") #Se posiciona en la ventana
+        self.Entry_nombre = Entry(self.ventana, width=25, bg="light green") #Caja de texto para el nombre
+        self.Entry_nombre.place (x= 310,y= 230,anchor="center") #Se posiciona en la ventana
+        self.Label_text_creacion = Label (self.ventana, text="Por favor introduce la fecha de creación del robot:", bg = "#67B59C", fg = "black") #Label con un texto
+        self.Label_text_creacion.place (x= 310,y=260,anchor="center") #Se posiciona en la ventana
+        self.Entry_creacion = Entry(self.ventana, width=25, bg="light green") #Caja de texto para la creación
+        self.Entry_creacion.place (x= 310,y= 290,anchor="center") #Se posiciona en la ventana
+        self.Label_text_modelo = Label (self.ventana, text="Por favor introduce un modelo al robot:", bg = "#67B59C", fg = "black") #Label con un texto
+        self.Label_text_modelo.place (x= 310,y=320,anchor="center") #Se posiciona en la ventana
+        self.Entry_modelo = Entry(self.ventana, width=25, bg="light green") #Caja de texto para el modelo
+        self.Entry_modelo.place (x= 310,y= 350,anchor="center") #Se posiciona en la ventana
+        self.Label_text_fabricante = Label (self.ventana, text="Por favor introduce el nombre del fabricante:", bg = "#67B59C", fg = "black") #Label con un texto
+        self.Label_text_fabricante.place (x= 310,y=380,anchor="center") #Se posiciona en la ventana
+        self.Entry_fabricante = Entry(self.ventana, width=25, bg="light green") #Caja de texto para el nombre del fabricante
+        self.Entry_fabricante.place (x= 310,y= 410,anchor="center") #Se posiciona en la ventana
         self.ventana.mainloop() #El loop principal de toda la pantalla principal
     
 
@@ -105,21 +103,21 @@ class iniciador:
 
         """
 
-        self.root = Toplevel()
+        self.root = Toplevel() #Se crea la ventana secundaria
         self.root.title("ROBOT") #Titulo de la pantalla principal. 
         self.root.minsize(600,700) #Dimensiones de la pantalla.
         self.root.resizable(width=NO,height=NO) #Que no se puede modificar. 
-        self.fondo = iniciador.cargarImagen(self,"robot_fondo2.gif")
-        self.canvas = Canvas(self.root, width=700, height=700, bg="purple")
-        self.canvas.place(x=0, y=0)
-        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw")
-        self.Boton_atras = Button(self.root,text="ATRÁS",bg="green",fg="yellow",height= 2,width=7,command= self.root.destroy)
-        self.Boton_atras.place(x=308,y=450, anchor = "center")
-        self.Boton_escenario1 = Button(self.root,text="ESCENARIO 1",bg="green",fg="yellow",height= 2,width=10)
-        self.Boton_escenario1.place(x=210,y=300, anchor = "center")
-        self.Boton_escenario2 = Button(self.root,text="ESCENARIO 2",bg="green",fg="yellow",height= 2,width=10)
-        self.Boton_escenario2.place(x=410,y=300, anchor = "center")
-        self.root.mainloop()
+        self.fondo = iniciador.cargarImagen(self,"robot_fondo2.gif") #Imagen de fondo
+        self.canvas = Canvas(self.root, width=700, height=700, bg="purple") #Se inicia el canvas
+        self.canvas.place(x=0, y=0) #Se posiciona
+        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw") #Se crea la imagen
+        self.Boton_atras = Button(self.root,text="ATRÁS",bg="green",fg="yellow",height= 2,width=7,command= self.root.destroy) #Boton para regresarse
+        self.Boton_atras.place(x=308,y=450, anchor = "center") #Se posiciona en la pantalla
+        self.Boton_escenario1 = Button(self.root,text="ESCENARIO 1",bg="green",fg="yellow",height= 2,width=10) #Botón para el primer escenario
+        self.Boton_escenario1.place(x=210,y=300, anchor = "center") #Se posiciona en la pantalla
+        self.Boton_escenario2 = Button(self.root,text="ESCENARIO 2",bg="green",fg="yellow",height= 2,width=10) #Botón para el segundo escenario
+        self.Boton_escenario2.place(x=410,y=300, anchor = "center") #Se posiciona en la pantalla
+        self.root.mainloop() #Mainloop de la ventana
     
     def about(self):
         """
@@ -135,19 +133,16 @@ class iniciador:
 
         """
 
-        self.root = Toplevel()
-        self.root.title("ABOUT") #Titulo de la pantalla principal. 
+        self.root = Toplevel() #Se crea la vetana secundaria
+        self.root.title("ABOUT") #Titulo de la pantalla secundaria. 
         self.root.minsize(600,700) #Dimensiones de la pantalla.
         self.root.resizable(False,False) #Que no se puede modificar. 
-        self.fondo = iniciador.cargarImagen(self,"robot_fondo2.gif")
-        self.info = iniciador.cargarImagen(self,"info.gif")
-        self.canvas = Canvas(self.root, width=700, height=700, bg="purple")
-        self.canvas.place(x=0, y=0)
-        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw")
-        self.canvas.create_image(80, 180, image=self.info, anchor="nw")
-        self.Boton_return = Button(self.root,text="ATRAS",bg="red",fg="yellow",height= 2,width=7, command=self.root.destroy)
-        self.Boton_return.place(x=308,y=490, anchor = "center")
-        self.root.mainloop()
-
-
-iniciador()
+        self.fondo = iniciador.cargarImagen(self,"robot_fondo2.gif") #Se pone la imagen
+        self.info = iniciador.cargarImagen(self,"info.gif") #Se pone la otra imagen
+        self.canvas = Canvas(self.root, width=700, height=700, bg="purple") #Se crea el canvas
+        self.canvas.place(x=0, y=0) #Se posiciona el canvas
+        self.canvas.create_image(0, 0, image=self.fondo, anchor="nw") #Se crea la imagen
+        self.canvas.create_image(80, 180, image=self.info, anchor="nw") #Se crea la imagen
+        self.Boton_return = Button(self.root,text="ATRAS",bg="red",fg="yellow",height= 2,width=7, command=self.root.destroy) #Botón para regresar
+        self.Boton_return.place(x=308,y=490, anchor = "center") #Se posiciona en la pantalla
+        self.root.mainloop() #Mainloop de la pantalla
